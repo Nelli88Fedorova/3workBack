00f1445c0cf7cb809f25ddd022953e78d8f2a752
Нелли Федорова <90643474+Nelli88Fedorova@users.noreply.github.com>
2022-03-22 19:44:00 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset= "UTF-8">
+<link rel="stylesheet" href="stiles.css">
+<title>3BackEnd</title>
+<style>
+.for
+{
+font-size: x-large;
+text-align: center;
+max-width:420px;
+border: 2px solid rgb(26, 18, 144);
+margin: 0 auto;
+margin-top:50px;
+}
+input
+{
+ margin-top:10px;
+ margin-bottom:10px;
+ font-size: x-large;
+ }
+ option
+ {
+  font-size: x-large;
+ }
+
+</style>
+
+</head>
+<body>
+    <div class="for">
+    <h4><form action="" method="POST">
+      <label>
+               Имя:<br />
+               <input name="name"
+                 value="-" />
+             </label><br />
+       
+             <label>
+              e-mail:<br />
+               <input name="email"
+                 value="-"
+                 type="email" />
+             </label><br />
+       
+             <label>
+               Дата рождения:<br />
+               <input name="date"
+                 value="-"
+                 type="date" />
+             </label><br />
+             
+             Пол:<br />
+             <label><input type="radio" checked="checked"
+               name="gender" value="m" />
+               М</label>
+             <label><input type="radio"
+               name="gender" value="w" />
+               Ж</label><br />
+       
+       Количество конечностей:<br />
+             <label><input type="radio" checked="checked" name="hand" value="1" />1</label>
+             <label><input type="radio" name="hand" value="2" /> 2</label>
+             <label><input type="radio" name="hand" value="3" /> 3</label>
+             <label><input type="radio" name="hand" value="4" /> 4</label>
+             <label><input type="radio" name="hand" value="5" /> 5</label>
+       
+             <label>
+               Сверхспособности:<br />
+               <select name="syperpover []" multiple>
+                 <option selected="selected" value="immortality">бессмертие</option>
+                 <option value="passing through walls" >прохождение сквозь стены</option>
+                 <option value="levitation">левитация</option>
+               </select>
+             </label><br />
+       
+       <label>
+               Биография:<br />
+               <textarea name="biography"></textarea>
+             </label><br />
+             
+             <br />
+             <label><input type="checkbox" checked="checked"
+               name="check" />
+               С контрактом ознакомлен(а) </label><br />
+             
+<input type="submit" value="Отправить" />
+</form></h4>
+</div>
+</body>
+</html>
+
---
+++ send.php
@@ -0,0 +1,81 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+if($_SERVER['REQUEST_METHOD']=='GET')
+{
+if(!empty($_GET['save']))
+{
+    print('Данные сохранены');
+}
+include('index.php');
+exit();
+}
+$flag=FALSE;
+
+if(empty($_POST['name']))
+{
+    print('Введите имя!');
+    $flag=TRUE;
+}
+if(empty($_POST['email']))
+{
+    print('Введите электронную почту!');
+    $flag=TRUE;
+}
+if(empty($_POST['date']))
+{
+    print('Введите возраст!');
+    $flag=TRUE;
+}
+if(empty($_POST['gender']))
+{
+    print('Укажите пол!');
+    $flag=TRUE;
+}
+if(empty($_POST['hand']))
+{
+    print('Укажите количество конечностей!');
+    $flag=TRUE;
+}
+if(empty($_POST['syperpover']))
+{
+    print('Выберите суперспособнос(ти/ть)!');
+    $flag=TRUE;
+}
+if(empty($_POST['biography']))
+{
+    print('Расскажите о себе!');
+    $flag=TRUE;
+}
+if(empty($_POST['check']))
+{
+    print('Необходимо ваше согласие!');
+    $flag=TRUE;
+}
+if($glag){exit();}
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$date=$_POST['date'];
+$gender=$_POST['gender'];
+$hand=$_POST['hand'];
+$biography=$_POST['biography'];
+$check=$_POST['check'];
+$syperpover=implode(',',$_POST['select']);
+
+$user='u47586'; $pass='3927785';
+$db=new PDO('mysql:host=localhost;dbname=u47586',$user,$pass, array(PDO::ATTR_PERSISTENT=>true));
+
+try{
+    $stmt=$bd->prepare("INSERT INTO MainData SET name = ?, email = ?, date=?, gender=?, hand=?, check=?");
+    $stmt->execute(array($name, $email,$date, $gender,$hand, $check));
+
+    $super=$db->>prepare("INSERT INTO Superpovers SET power_id=?, powers=?");
+    $super->execute(array($syperpover));
+}
+catch(PDOException $e)
+{
+    print('Error:'.$e->>GetMessage());
+    exit();
+}
+header('Location: ?save=1');
+  ?>
